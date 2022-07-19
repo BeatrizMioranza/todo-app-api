36dc4c515786bd4eaf3f450f6f667b9a7557ac76
BeatrizMioranza <beatrizmioranza2508@gmail.com>
2022-07-18 21:29:13 -0300

    adicionei o post em taferas e usuario

--- src/controller/tarefacontroller.js
+++ src/controller/tarefacontroller.js
@@ -1,6 +1,9 @@
-const tarefaController = (api) =>
-   api.get('/tarefa', (req, res) => {
-  res.send('Rota GET para tarefa')
-})
-
+const tarefaController = (api) => {
+  api.get('/tarefa', (req, res) => {
+    res.send('Rota GET para tarefa')
+  })
+  api.post('/tarefa', (req, res) => {
+    res.send('Rota post para tarefa')
+  })
+}
 export default tarefaController
--- src/controller/usuariocontroller.js
+++ src/controller/usuariocontroller.js
@@ -1,6 +1,9 @@
-const usuarioController = (api) =>
+const usuarioController = (api) =>{
     api.get('/usuario', (req, res) => {
     res.send('Rota GET para usuario')
   })
-
+  api.post('/usuario', (req, res) => {
+    res.send('Rota post para usuario')
+  })
+}
   export default usuarioController
